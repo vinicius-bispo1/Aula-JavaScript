1715a333d24b748ea474ea993e2139c7e9569914
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-11-04 19:52:26 -0300

    🎉 Commit inicial

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Bom dia!!!!!!!!!!!!!!!!!")
+
+// function nomeBomdia(){
+//     // codigo a ser executado
+// }
+
+function bomdia(){
+    console.log("Bom dia a todos!")
+}
+bomdia()
+
+
+let info = function(nome, idade, cidade){
+    console.log(nome)
+    console.log(`Sua idade é ${idade}`)
+    console.log(cidade)
+    console.log(nome, idade, cidade)
+}
+
+info("Kairo", 24, "Salvador")
+
+
+let frutas = "morango"
+let dia = "lindo"
+
+function mostraFruta(fruta){
+    console.log(fruta)
+}
+
+mostraFruta(dia)
+// mostraFruta("morango")
+
+function soma(n1, n2){
+     return n1 + n2
+}
+
+console.log(soma(3,5))
+
+const somaDia = (nome) => {
+    console.log(nome)
+}
+somaDia("Vini")
+
+const soma = (n1,n2) => n1 + n2
+console.log(soma(2,5))
+console.log(soma(5,5))
+console.log(soma(8,1))
+
